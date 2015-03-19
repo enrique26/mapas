9eedc63135227a3129d7454344cd0964cdc1e7c5
Enrique26 <enrique@negoapps.com>
2015-03-19 15:28:11 -0600

    creacion de mapas y geolocalizacion titanium

---
+++ app/controllers/index.js
@@ -0,0 +1,13 @@
+var Map = require('ti.map');
+
+
+var mapview = Map.createView({
+    mapType: Map.NORMAL_TYPE,
+    region: {latitude:33.74511, longitude:-84.38993,
+            latitudeDelta:0.01, longitudeDelta:0.01},
+    animate:true,
+    regionFit:true,
+    userLocation:true,
+});
+$.index.add(mapview);
+$.index.open();
